812b6033ffdab09e42915fe3c848e7da2938c7bf
Umair Waseem <umair.waseem.q@gmail.com>
2021-02-16 23:37:47 +0500

    animation

---
+++ index.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('.car-color').click(function() {
+
+        var image = $(this).data().img;
+
+        console.log(image);
+
+        $('.selected-car1').fadeOut(500, function() {
+            $('.selected-car1').attr('src', 'assets/h5/' + image);
+        });
+
+        $('.selected-car1').fadeIn(500);
+    })
+})
