6e9982d74b1b3a490fe7d03e8117ad941fb76ef0
Umair Waseem <umair.waseem.q@gmail.com>
2021-02-19 23:07:37 +0500

    Animationfixed

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
     $('.car-color').click(function() {
 
@@ -15,8 +16,42 @@ $(document).ready(function() {
     var jsHeight = document.documentElement.clientHeight;
     console.log(jsHeight);
 
+    $(window)
+    .resize(function () {
+      window.WINDOW_WIDTH = $(window).width();
+      window.WINDOW_HEIGHT = $(window).height();
+      window.ISWAP = window.WINDOW_WIDTH < 750;
+      window.isWAP768 = window.WINDOW_WIDTH <= 768;
+      
+      if (!window.isWAP768) {
+        var imgSize = $(window).width() / 1920;
+        $("#loading-wrap").css({
+          transform: "scale(" + imgSize + ")",
+          "transform-origin": " 0 0",
+          height: $(window).height() + 1080 * imgSize,
+        });
+      } else {
+        var imgSize = $(window).height() / 1080;
+        $("#loading-wrap").css({
+          transform: "scale(" + imgSize + ") translateX(-50%)",
+          "transform-origin": " 0 0",
+        });
+      }
+    })
+    .resize();
+
     var loader = initLoading(document.getElementById('loader'));
 
     loader.setP(100);
+
+    loader.addEventListener("onAniEvt2",function() {
+        $('body').removeClass('overflow');
+        $('section, header').css('opacity', '1');
+        $('#loading-wrap').fadeOut();
+    });
+
+    
+
+    
 
 })
